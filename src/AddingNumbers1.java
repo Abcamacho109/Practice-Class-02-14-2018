95efc53314d03a6637d0f35cbb82b627af06b09a
Camacho <abcamacho.2017@mymail.jcsu.edu>
2018-02-14 15:48:04 -0500

    first commit

---
+++ src/AddingNumbers1.java
@@ -0,0 +1,16 @@
+
+public class AddingNumbers1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a = 6;
+int b = 21;
+int c = addnumbers(a,b);
+System.out.println("The sum is" + c);
+
+	}
+public static int addnumbers(int num1, int num2){
+	int sum = num1 + num2;
+	return sum;
+}
+}
